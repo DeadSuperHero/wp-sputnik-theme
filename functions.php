c0a0981832f82265ca74f584f017205189c25243
DeadSuperHero <sean@deadsuperhero.com>
2018-12-27 14:34:38 -0800

    revert last commit

--- functions.php
+++ functions.php
@@ -166,12 +166,6 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-function cc_mime_types($mimes) {
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
-
 function prefix_category_title( $title ) {
     if ( is_category() ) {
         $title = single_cat_title( '', false );
