19fb4741b9a30d9bcf0ae4dba013f41eadeb1f6c
deadsuperhero <sean@deadsuperhero.com>
2019-04-10 13:27:52 -0700

    try having no comments

--- template-parts/content.php
+++ template-parts/content.php
@@ -60,14 +60,5 @@
 			) );
 			?>
 		</div><!-- .entry-content -->
-		<?php
-		// If comments are open or we have at least one comment, load up the comment template.
-		if ( comments_open() || get_comments_number() ) :
-			comments_template();
-		endif;
-
-	endwhile; // End of the loop.
-	?>
-
 	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
