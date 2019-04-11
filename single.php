57a89e7308605782d0648ce5e03d97876bcdcf01
deadsuperhero <sean@deadsuperhero.com>
2019-04-11 11:04:51 -0700

    try putting related posts inline to layout

--- single.php
+++ single.php
@@ -21,43 +21,6 @@ get_header();
 		endwhile; // End of the loop.
 		?>
 
-	<?php
-		$tags = wp_get_post_tags($post->ID);
-		if ($tags) {
-		$tag_ids = array();
-		foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
-
-		$args=array(
-		'tag__in' => $tag_ids,
-		'post__not_in' => array($post->ID),
-		'showposts'=>4,  // Number of related posts that will be shown.
-		'caller_get_posts'=>1
-		);
-
-		$my_query = new wp_query($args);
-		if( $my_query->have_posts() ) {
-		echo '<div id="relatedposts"><h3>Related Posts</h3><ul>';
-		while ($my_query->have_posts()) {
-		$my_query->the_post();
-		?>
-
-		<?php
-		if ( has_post_thumbnail() ) { ?>
-		<li class="relatedthumb"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?><?php the_title(); ?></a></li>
-		<?php } else { ?>
-		<li class="relatedthumb"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><img src="<?php echo get_post_meta($post->ID, 'Image',true) ?>" width="196" height="110" alt="<?php the_title_attribute(); ?>" /><?php the_title(); ?></a></li>
-		<?php }
-		?>
-
-		<?php
-		}
-		echo '</ul>';
-		}
-		}
-		$post = $backup;
-		wp_reset_query();
-		?>
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
