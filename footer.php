cafc0c01a2a64f1956abe4fd4d5acd97daba66bf
Sean Tilley <sean@deadsuperhero.com>
2019-11-07 14:35:15 -0600

    add feneas logo

--- footer.php
+++ footer.php
@@ -15,6 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
+			<img class="feneas-logo" src="../images/feneas-small.png">feneas-logo</img>
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'We Distribute is proudly part of', 'sputnik', 'a non-profit organization dedicated to decentralized technologies and federated communication systems. We are working together to provide knowledge and services to help make the fediverse grow!'  ), 'sputnik', '<a href="https://feneas.org">Federated Networks Association ry</a>' );
