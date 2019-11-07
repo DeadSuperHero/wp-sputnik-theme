291169572cbe17ee8ebde902a3114c3040ecd7bc
Sean Tilley <sean@deadsuperhero.com>
2019-11-07 14:37:20 -0600

    do it the cheap way

--- footer.php
+++ footer.php
@@ -16,10 +16,7 @@
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<img class="feneas-logo" src="../images/feneas-small.png">feneas-logo</img>
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'We Distribute is proudly part of', 'sputnik', 'a non-profit organization dedicated to decentralized technologies and federated communication systems. We are working together to provide knowledge and services to help make the fediverse grow!'  ), 'sputnik', '<a href="https://feneas.org">Federated Networks Association ry</a>' );
-				?>
+			<p>We Distribute is proudly part of <a href="https://feneas.org/">Federated Networks Association ry</a> a non-profit organization dedicated to decentralized technologies and federated communication systems. We are working together to provide knowledge and services to help make the fediverse grow!</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
